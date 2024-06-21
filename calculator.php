b882d680e612b2fbd7b67aea24aef582c0d9e525
johnvillamor <131235472+johnvillamor@users.noreply.github.com>
2024-06-21 16:12:22 +0800

    sqrt operation added

--- calculator.php
+++ calculator.php
@@ -1,20 +1,36 @@
 <?php
 
+//Variable declarations
 $num1 = readline("Enter first number: ");
-$num2 = readline("Enter second number: ");
-$operator = readline("operator: ");
+$operator = readline("Choose an operator (+, -, *, /, sqrt): ");
 
-if ($operator == '+') {
-    $result = $num1 + $num2;
-} else if ($operator == '-') {
-    $result = $num1 - $num2;
-} else if ($operator == '*') {
-    $result = $num1 * $num2;
-} else if ($operator == '/') {
-    $result = $num1 / $num2;
+//Condition if you choose the sqrt operator
+if ($operator == 'sqrt') {
+    $result = sqrt($num1);
 } else {
-    echo "Invalid operator";
-    exit(1);
+    $num2 = readline("Enter second number: ");
+    
+//Conditions for +, -, *, / operators
+    if ($operator == '+') {
+        $result = $num1 + $num2;
+    } else if ($operator == '-') {
+        $result = $num1 - $num2;
+    } else if ($operator == '*') {
+        $result = $num1 * $num2;
+    } else if ($operator == '/') {
+
+ //Condition if the number is divided by zero       
+        if ($num2 == 0) {
+            echo "Error: Division by zero.";
+            exit(1);
+        }
+        $result = $num1 / $num2;
+
+ //Condition if the operators entered invalid           
+    } else {
+        echo "Invalid operator";
+        exit(1);
+    }
 }
 
 echo $result;
